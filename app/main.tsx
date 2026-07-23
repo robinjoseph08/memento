5da4c528089ea32ad6a0dd119c76e84f59ee3b5e
Robin Joseph <robinmjoseph08@gmail.com>
2026-07-23 15:02:05 -0500

    [Frontend] Use DM Serif Display for headings

--- app/main.tsx
+++ app/main.tsx
@@ -2,6 +2,7 @@ import "@fontsource/dm-sans/400.css";
 import "@fontsource/dm-sans/500.css";
 import "@fontsource/dm-sans/600.css";
 import "@fontsource/dm-sans/700.css";
+import "@fontsource/dm-serif-display/400.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
